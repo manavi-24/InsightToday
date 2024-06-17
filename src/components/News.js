e8386ab73d9218e553c74434d751aab7d3b16555
Manavi Sharma <150844157+manavi-24@users.noreply.github.com>
2024-06-17 13:11:03 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -23,26 +23,21 @@ const News=(props)=> {
         setLoading(true)
         let data=await fetch(url);
         let parsedData = await data.json()
-        props.setProgress(50)
+        props.setProgress(50);
         setArticles(parsedData.articles)
         settotalResults(parsedData.totalResults)
         setLoading(false)
-        props.setProgress(100)}
+        props.setProgress(100);}
 
         useEffect(() => {
           document.title=`InsightToday- ${capitalizeFirstLetter(props.category) } News`
           updateNews();
         }, [])
 
-     const handleNextClick=async()=>{
-        setPage(page+1)
-        updateNews();}
 
-      const handlePrevClick=async()=>{
-      setPage(page-1)
-      updateNews();}
 
     const fetchMoreData = async() => {
+     
       let url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`
       setPage(page+1)
       let data=await fetch(url);
@@ -57,7 +52,8 @@ const News=(props)=> {
         <>
         <div className="container my-3">
           <h2 className="text-center" style={{ margin: '36px, 0px', marginTop:"70px"}}>Insight Today- Top Headlines  for {capitalizeFirstLetter(props.category)}</h2>
-          <InfiniteScroll
+          
+        <InfiniteScroll
           dataLength={articles.length}
           next={fetchMoreData}
           hasMore={articles.length!==totalResults}
@@ -68,7 +64,7 @@ const News=(props)=> {
               {articles.map((element)=>{
                   return(
                    <div className="col-sm-12 col-md-6 col-lg-4 mb-3" key={element.url} >
-                   <Newsitem title={element.title} description={element.description} imageURL={element.urlToImage} 
+                   <Newsitem title={element.title?element.title:"News"} description={element.description} imageURL={element.urlToImage} 
                    newsURL={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
                  </div>)
   
@@ -77,6 +73,31 @@ const News=(props)=> {
           </div>
           </InfiniteScroll>
           
+        {/* {articles && articles.length > 0 ? (
+  <InfiniteScroll
+    dataLength={articles.length}
+    next={fetchMoreData}
+    hasMore={articles.length !== totalResults}
+    loader={<div className="text-center"><Spinner /></div>}
+    
+  >
+    <div className="container text-center ">
+            <div className="row text-center">
+              {articles.map((element)=>{
+                  return(
+                   <div className="col-sm-12 col-md-6 col-lg-4 mb-3" key={element.url} >
+                   <Newsitem title={element.title?element.title:"News"} description={element.description} imageURL={element.urlToImage} 
+                   newsURL={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
+                 </div>)
+  
+              })}
+             </div>
+          </div>
+  </InfiniteScroll>
+) : (
+  <div className="text-center"><Spinner /></div>
+)}*/}
+
         </div>
         </>
         );
@@ -94,7 +115,8 @@ News.defaultProps={
 News.propTypes={
   country: PropTypes.string,
   pageSize: PropTypes.number,
-  category: PropTypes.string
+  category: PropTypes.string,
+
 }
 
 export default News;
